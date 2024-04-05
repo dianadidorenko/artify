76c00da3ff41780341e7ef31ad89c3384a3ba0a4
Diana Didorenko <125818555+dianadidorenko@users.noreply.github.com>
2024-04-05 16:35:31 +0300

    img path fix 2

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -28,7 +28,7 @@ export async function POST(req) {
     const bytes = await file.arrayBuffer();
     const buffer = Buffer.from(bytes);
 
-    const profileImagePath = `/uploads${file.name}`;
+    const profileImagePath = `/public/uploads${file.name}`;
     await writeFile(profileImagePath, buffer);
 
     console.log(`open ${profileImagePath} to see the uploaded files`);
